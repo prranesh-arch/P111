7ff4fe70c020574c5f172775a05fde7e753c9d9c
prranesh-arch <81365988+prranesh-arch@users.noreply.github.com>
2022-05-18 11:31:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+prediction = "";
+
+Webcam.set({
+  width:350,
+  height:300,
+  image_format : 'png',
+  png_quality:100
+});
+
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot() { 
+  Webcam.snap(function(data_uri) {
+    document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+});
+}
+
+ console.log('ml5 version:', ml5.version);
+
+ classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/VmXZuIhKT/model.json',modelLoaded);
+
+ function modelLoaded(){
+    console.log('Model Loaded');
+ }
+
+ function speak(){
+  var synth = window.speechSynthesis;
+  speak_data_1 = "The prediction is " + prediction;
+  var utterThis = new SpeechSynthesisUtterance(speak_data_);
+  synth.speak(utterThis);
+ }
